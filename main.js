6dee9a0795b8e14e0c0968a0fbdb12507c30c32e
Alysson Barrera <86618257+alyssonbarrera@users.noreply.github.com>
2022-05-07 12:15:11 -0300

    Update main.js

--- main.js
+++ main.js
@@ -50,6 +50,7 @@ function showNavOnScroll() {
 function showBackToTopButtonOnScroll() {
   if(scrollY > 550) {
     backToTopButton.classList.add('show')
+  } else {
     backToTopButton.classList.remove('show')
   }
 }
